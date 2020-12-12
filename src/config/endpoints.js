a254775d7a66ef39674cabfe83187400a3743976
jfernandezpe <jfernandezpe.dev@gmail.com>
2020-12-12 23:53:08 +0100

    chore: production API configuration

---
+++ src/config/endpoints.js
@@ -0,0 +1,12 @@
+import { isProduction } from './environment.js';
+
+// TODO: find out another way to set the url
+const prefix = isProduction()
+  ? 'https://simple-login-app-ee195-default-rtdb.europe-west1.firebasedatabase.app'
+  : '/api';
+
+  const subfix = isProduction() ? '.json' : '';
+
+export const endpoints = {
+  authentication: `${prefix}/authentication${subfix}`,
+};
